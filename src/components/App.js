33be5106d48adf284674e3083ae509220d279981
elfrmkr <elfrmkr98@gmail.com>
2021-10-16 13:25:22 +0300

    providing consumer to get info from mutliple context objects inside of a single component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import UserCreate from "./userCreate";
 import languageContext from "../contexts/languageContext";
+import colorContext from "../contexts/colorContext";
 
 class App extends React.Component {
   state = { language: "english" };
@@ -8,25 +9,28 @@ class App extends React.Component {
   onLanguageChange = (language) => {
     this.setState({ language: language });
   };
+
   render() {
     return (
-      <div className="ui container"  style={{padding:"12px"}}>
+      <div className="ui container" style={{ padding: "12px" }}>
         <div>
-          Select a language: 
+          Select a language:
           <i
             className="flag us"
-            onClick={() => this.onLanguageChange("english") }
-            style={{paddingLeft:"5px"}}
+            onClick={() => this.onLanguageChange("english")}
+            style={{ paddingLeft: "5px" }}
           />
           <i
             className="flag tr"
             onClick={() => this.onLanguageChange("turkish")}
-            style={{paddingLeft:"4px"}}
+            style={{ paddingLeft: "4px" }}
           />
         </div>
-        <languageContext.Provider value= {this.state.language}>
+        <colorContext.Provider value="red">
+          <languageContext.Provider value={this.state.language}>
             <UserCreate />{" "}
-        </languageContext.Provider>
+          </languageContext.Provider>
+        </colorContext.Provider>
       </div>
     );
   }
--- src/components/button.js
+++ src/components/button.js
@@ -1,16 +1,31 @@
 import React from "react";
 import languageContext from "../contexts/languageContext";
+import colorContext from "../contexts/colorContext";
 
 class Button extends React.Component {
-// special property name
-// static adds a property to the class itself
-  static contextType = languageContext;
+  // special property name
+  // static adds a property to the class itself
+  /***static contextType = languageContext;
 /* this is same as saying
 Button.contextType = languageContext 
 */
- render() {
-   const text = this.context === 'english'? 'Submit' : 'Gönder'
-    return <button className="ui button primary">{text}</button>;
+  render() {
+    //const text = this.context === 'english'? 'Submit' : 'Gönder'
+    return (
+      // providing function as a child in a react component while using Consumer
+      /*
+      We use consumer to get info from mutliple context objects inside of a single component
+      */
+      <colorContext.Consumer>
+        {(color) => (
+          <button className={`ui button ${color}`}>
+            <languageContext.Consumer>
+              {(value) => (value === "english" ? "Submit" : "Gönder")}
+            </languageContext.Consumer>
+          </button>
+        )}
+      </colorContext.Consumer>
+    );
   }
 }
 
