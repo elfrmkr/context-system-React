5a90b8e2fb9b24cc4af080d7a4882bacb8cff5ca
elfrmkr <elfrmkr98@gmail.com>
2021-10-16 12:40:23 +0300

    context property of the language is binded to the button class

--- src/components/button.js
+++ src/components/button.js
@@ -1,7 +1,14 @@
 import React from "react";
+import languageContext from "../contexts/languageContext";
 
 class Button extends React.Component {
-  render() {
+// special property name
+// static adds a property to the class itself
+  static contextType = languageContext;
+/* this is same as saying
+Button.contextType = languageContext 
+*/
+ render() {
     return <button className="ui button primary">Submit</button>;
   }
 }
---
+++ src/contexts/languageContext.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default React.createContext('english'); 
+// creating context object, default selected lang is english
+// arrays, objects, numbers can be passed through context
